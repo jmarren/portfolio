79b6482145d5dd0f826b359b291e095e6aa52f38
John Marren <jmarren99@gmail.com>
2023-09-11 19:40:04 -0700

    made nav vertical

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -7,9 +7,9 @@ interface NavbarProps {
 
 const Navbar: FC<NavbarProps> = ({ links }) => {
   return (
-<div className="flex justify-evenly pl-[10%] pr-[10%] relative top-0 ">
+<div className="grid grid-rows-4 justify-center pl-[10%] pr-[10%] relative top-0">
      {links.map((link, index) => (
-          <button key={index} style={{top: index * 2, fontFamily: "Sniglet"}} className="bg-white rounded-md h-[3rem] w-[10rem] text-barber-blue">
+          <button key={index} style={{top: index * 2, fontFamily: "Sniglet" }} className="bg-white col-span-full rounded-md h-[6rem] w-[10rem] text-barber-blue">
             <a href={link.url} style={{ textDecoration: 'none' }}>
               {link.name}
             </a>
