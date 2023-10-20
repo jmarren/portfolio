5c7ae29fdf8f2d5975e2f136f0dd84e49a0de12b
John Marren <jmarren99@gmail.com>
2023-10-20 13:17:34 -0500

    home colors

--- app/page.tsx
+++ app/page.tsx
@@ -6,15 +6,15 @@ import Layout from '../components/Home-Layout'
 
 
 export default function Home() {
-
-
+//bg-[#9ec9dd] Blue-gray
+// bg-[#74c36a] Green
     return (
 
 
         <div className="w-full min-h-screen ">
             <div className='parkay-floor h-full w-full fixed z-0'>
             </div>
-            <div className='bg-slate-300 z-[20] w-5/6 h-full min-h-screen relative left-[8.3vw] top-4 rounded-3xl ' id="LayoutContainer"  >    
+            <div className=' z-[20] ring ring-sky-300 bg-sky-200 w-5/6 h-full min-h-screen relative left-[8.3vw] top-4 rounded-3xl ' id="LayoutContainer"  >    
                 <Layout />
                 </div>
             {/* <div className='z-[200]  absolute'>
